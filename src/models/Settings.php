43902b49d8532509cf46e218707028f7c5fab293
Sigurd Heggemsnes <36757859+heggemsnes@users.noreply.github.com>
2020-07-29 14:15:03 +0200

    Update so dist path is correctly relative to current site url

--- src/models/Settings.php
+++ src/models/Settings.php
@@ -7,7 +7,7 @@ use craft\base\Model;
 class Settings extends Model
 {
     public $manifestPath = 'dist/parcel-manifest.json';
-    public $distPath = 'dist';
+    public $distPath = '/dist';
 
     public function rules()
     {
